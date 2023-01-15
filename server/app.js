197a3f051b4d77e9038c67cd24d5aeac2b0b4be0
Chalaka Hasantha Prasad <59790911+chalakahasanthaprasad@users.noreply.github.com>
2023-01-16 01:33:47 +0530

    direct add mongodb

--- server/app.js
+++ server/app.js
@@ -36,10 +36,13 @@ app.use("/api/favorite", favoriteFoodRouter);
 // Mongo DB Connections
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGO_DB_CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://chalaka95:29Q1AlTiH8EQF3t5@cluster0.ovijmw2.mongodb.net/iOS-FoodApp?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Successfully connected to mongodb !"))
   .catch((err) => console.log(`Error has occured: ${err}`));
 
